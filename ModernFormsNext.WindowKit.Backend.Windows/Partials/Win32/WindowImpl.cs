c8c94bbf8a0b90520e6d8c5efaa2d042c434b23a
Kiko kiko <ssasske08@gmail.com>
2026-03-28 15:06:52 +0100

    Add missing file

---
+++ ModernFormsNext.WindowKit.Backend.Windows/Partials/Win32/WindowImpl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using ModernFormsNext.WindowKit.Input;
+using ModernFormsNext.WindowKit.Platform;
+using ModernFormsNext.WindowKit.Threading;
+using ModernFormsNext.WindowKit.Backend.Windows.Win32.Interop;
+
+namespace ModernFormsNext.WindowKit.Backend.Windows.Win32
+{
+    internal partial class WindowImpl
+    {
+        public IEnumerable<object> Surfaces => new object[] { Handle, /* _gl, */ _framebuffer };
+
+        public void SetIcon(SkiaSharp.SKBitmap? icon)
+        {
+            if (icon == null)
+            {
+                UnmanagedMethods.PostMessage(_hwnd, (int)UnmanagedMethods.WindowsMessage.WM_SETICON,
+                    new IntPtr((int)UnmanagedMethods.Icons.ICON_BIG), IntPtr.Zero);
+
+                return;
+            }
+
+            using var icon2 = icon.ToBitmap();
+
+            UnmanagedMethods.PostMessage(_hwnd, (int)UnmanagedMethods.WindowsMessage.WM_SETICON,
+                new IntPtr((int)UnmanagedMethods.Icons.ICON_BIG), icon2.GetHicon());
+        }
+    }
+}
